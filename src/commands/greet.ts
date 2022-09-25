e9f46dceea958e283fffb599419d98576e1e4992
Kapeet <74069208+Kapeet@users.noreply.github.com>
2022-09-25 22:45:17 +0300

    add eslint config (#4)
    * add eslint config
    
    * Adding prettier related packages
    
    * eslint fixes

--- src/commands/greet.ts
+++ src/commands/greet.ts
@@ -1,8 +1,9 @@
-import {SlashCommandBuilder} from '@discordjs/builders';
+import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 
 export const data = new SlashCommandBuilder().setName('greet').setDescription('Holiday-appropriate greeting');
 
-export async function execute(interaction: CommandInteraction) {
-    return interaction.reply("! <@"+interaction.user+"> ,ראש השנה שמח");
+export async function execute (interaction: CommandInteraction): Promise<void> {
+  const hebrewText: string = 'ראש השנה שמח';
+  return await interaction.reply(`${interaction.user.toString()}, ${hebrewText}`);
 }
--- src/rest-api.ts
+++ src/rest-api.ts
@@ -1,46 +1,60 @@
-import {Client, ThreadChannel} from 'discord.js'
-import express from 'express'
+import { Client, ThreadChannel } from 'discord.js'
+import express, { Request, Response } from 'express'
 
-export function createRestApi(client: Client) {
-    const app = express();
-    app.use(express.json())
+export function createRestApi (client: Client): express.Application {
+  const app = express();
+  app.use(express.json())
 
-    app.get('/messages', async (req, res) => {
-        const {threadId} = req.query;
-        if (!threadId) return res.status(400).send('missing threadId')
+  app.get('/messages', (req: Request, res: Response): void => {
+    (async () => {
+      const { thread } = await getQueryParams(req, res, client);
 
-        const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+      const messages = await thread.messages.fetch();
+      const jsonResponse: string = JSON.stringify(messages ?? []);
+      res.status(200).send(jsonResponse);
+    })().catch(console.error);
+  })
 
-        if (!thread) res.status(404).send('Thread not found')
+  app.post('/message', (req: Request, res: Response): void => {
+    (async () => {
+      const { threadId, text } = req.body;
 
-        const messages = await thread.messages.fetch();
-        const jsonResponse: string = JSON.stringify(messages || []);
-        return res.status(200).send(jsonResponse);
-    })
+      const missingParams: boolean = threadId === null ?? threadId === undefined ?? text === null ?? text === undefined;
+      if (missingParams) return res.status(400).send('missing request params')
 
-    app.post('/message', async (req, res) => {
-        const { threadId, text } = req.body;
-        if (!threadId || !text) return res.status(400).send('missing request params')
+      const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
 
-        const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+      const missingThread: boolean = threadId === null ?? threadId === undefined;
+      if (missingThread) res.status(404).send('Thread not found')
+      await thread.send(text);
 
-        if (!thread) res.status(404).send('Thread not found')
-        await thread.send(text);
+      return res.status(200).send('Message sent.');
+    })().catch(console.error);
+  })
 
-        return res.status(200).send('Message sent.');
-    })
+  app.post('/resolve', (req: Request, res: Response): void => {
+    (async () => {
+      const { thread } = await getQueryParams(req, res, client);
 
-    app.post('/resolve', async (req, res) => {
-        const { threadId } = req.body;
-        if (!threadId) return res.status(400).send('missing request params')
+      await thread.send('This conversation is marked as resolved and this thread will be archived')
+      await thread.setArchived(true);
+    })().catch(console.error);
+  })
 
-        const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+  return app;
+}
 
-        if (!thread) res.status(404).send('Thread not found')
-        
-        await thread.send('This conversation is marked as resolved and this thread will be archived')
-        await thread.setArchived(true);
-    })
+async function getQueryParams (req: Request, res: Response, client: Client): Promise<{ thread: ThreadChannel, threadId: any }> {
+  const { threadId } = req.query;
+  if (threadId === null ?? threadId === undefined) {
+    res.status(400).send('missing threadId')
+  }
 
-    return app;
+  const thread = await client.channels.fetch(threadId as string) as ThreadChannel;
+
+  if (thread === null ?? threadId === undefined) {
+    res.status(404).send('Thread not found')
+  }
+
+  return { thread, threadId };
 }
