0f45d90dcdaa0e863d85878022de7d4e3b212361
Kapeet <itamar246@gmail.com>
2022-09-30 11:48:16 +0300

    unnecessary async

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { AudioResource, createAudioResource, StreamType } from '@discordjs/voice';
 import ytdl from 'ytdl-core';
 
-export async function getSongResourceByYouTubeUrl (url: string): Promise<AudioResource> {
+export function getSongResourceByYouTubeUrl (url: string): AudioResource {
   const stream = ytdl(url, {
     filter: 'audioonly',
     highWaterMark: 1 << 25
