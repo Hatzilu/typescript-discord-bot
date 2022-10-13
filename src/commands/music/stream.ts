bc130a9f27757a40cc053d26620ecfb9e9ca8f02
Kapeet <itamar246@gmail.com>
2022-10-13 22:18:03 +0300

    Add support for keywords instead of URLs

--- src/commands/music/stream.ts
+++ src/commands/music/stream.ts
@@ -1,10 +1,14 @@
+import ytdl from 'ytdl-core';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { AudioPlayerStatus } from '@discordjs/voice';
 import { Client, CommandInteraction, GuildMember, TextChannel, VoiceChannel } from 'discord.js';
-import ytdl from 'ytdl-core';
 import { Song } from '../../types';
 import { getSongResourceByYouTubeUrl } from '../../utils';
 import { serverQueue, player, connectToChannel } from './music-utils';
+import { config } from '../../config';
+import fetch from 'node-fetch';
+
+const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/search?key=${config.YOUTUBE_API_KEY}&type=video&q={QUERY}`;
 
 export const data = new SlashCommandBuilder().setName('stream').setDescription('stream a song in VC').addStringOption(option =>
   option
@@ -19,16 +23,25 @@ export async function execute (interaction: CommandInteraction, client: Client):
   if (serverQueue.getTextChannel() === undefined) {
     serverQueue.setTextChannel(interaction.channel as TextChannel);
   }
-  const url = interaction.options.getString('query');
-
-  console.log(url);
-  if (url === null) {
+  let queryUrlOrString = interaction.options.getString('query');
+  if (queryUrlOrString === null) {
     await interaction.editReply('please provide a url!');
     return;
   }
-  if (!ytdl.validateURL(url)) {
-    await interaction.editReply('Invalid YouTube url!');
-    return;
+  const queryIsKeywords = !ytdl.validateURL(queryUrlOrString) && typeof queryUrlOrString === 'string';
+  if (queryIsKeywords) {
+    const apiUrlWithQuery = youtubeApiUrl.replace('{QUERY}', queryUrlOrString);
+    const res = await fetch(apiUrlWithQuery);
+    const queryResults = await res.json() as youtubeResponse;
+    if (queryResults.items.length === 0) {
+      await interaction.editReply('something went wrong while fetching the query results!');
+      return;
+    }
+    if (queryResults.items[0].id.videoId === null) {
+      await interaction.editReply(`invalid query result! ${JSON.stringify(queryResults)}`);
+      return;
+    }
+    queryUrlOrString = `https://www.youtube.com/watch?v=${queryResults.items[0].id.videoId}`;
   }
   const connection = await connectToChannel(voiceChannel);
 
@@ -40,8 +53,8 @@ export async function execute (interaction: CommandInteraction, client: Client):
   }
 
   const newSong: Song = {
-    url,
-    info: await ytdl.getInfo(url),
+    url: queryUrlOrString,
+    info: await ytdl.getInfo(queryUrlOrString),
     requestingUser: interaction.user
   };
   serverQueue.addSongToQueue(newSong);
@@ -58,4 +71,25 @@ export async function execute (interaction: CommandInteraction, client: Client):
   }
 
   console.log('queued songs:', serverQueue.getQueuedSongs().length);
+}
+
+interface youtubeResponse {
+  kind: string
+  etag: string
+  nextPageToken: string
+  regionCode: string
+  pageInfo: {
+    totalResults: number
+    resultsPerPage: number
+  }
+  items: youtubeResponseItem[]
+}
+
+interface youtubeResponseItem {
+  kind: string
+  etag: string
+  id: {
+    kind: string
+    videoId: string
+  }
 }
