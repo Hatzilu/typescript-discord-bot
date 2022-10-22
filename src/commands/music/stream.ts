21ba1a0ab041993f929b5e589274e72a95e43c13
Kapeet <74069208+Kapeet@users.noreply.github.com>
2022-10-22 21:06:40 +0300

    Music bot (#9)
    * some progress
    
    * adding required deps for voice stuff to work
    
    * add VC intent
    
    * add missing packages
    
    * works with url only now
    
    * remove unnecessary dependency
    
    * new serverqueue class, added util function for getting audio resources by ytdl url
    
    * implementing serverqueue in stream command
    
    * unnecessary async
    
    * changing serverqueue to handle URL strings instead of AudioResource types, queueing multiple songs works now
    
    * eslint fix
    
    * Add Song interface to deal with handling song metadata, add a way to declare connection as const instead of let, adding multiple interaction.editReply commands to convey player status to the user
    
    * handle voice connection disconnects, now the bot doesn't crash if it leaves and rejoins VC
    
    * queue command, exporting serverQueue and AudioPlayer to be accessed anywhere
    
    * more accurate variable names
    
    * remove requested by since it displays wrong information
    
    * connectToVC function
    
    * fix bot skipping queue to last song, added requestingUser property of type User (discordjs) to Song interface
    
    * fixed song repeating, queueing different songs now works properly
    
    * make ServerQueue variables private with getters and setters, add textChannel variable, move player.on.playing event out of the execute command becuase it was causing issues.
    
    * rename url => query
    
    * test
    
    * Add support for keywords instead of URLs
    
    * fix voice connection crash, add more logging
    
    * even more logging

---
+++ src/commands/music/stream.ts
@@ -0,0 +1,96 @@
+import ytdl from 'ytdl-core';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { AudioPlayerStatus } from '@discordjs/voice';
+import { Client, CommandInteraction, GuildMember, TextChannel, VoiceChannel } from 'discord.js';
+import { Song } from '../../types';
+import { getSongResourceByYouTubeUrl } from '../../utils';
+import { serverQueue, player, connectToChannel } from './music-utils';
+import { config } from '../../config';
+import fetch from 'node-fetch';
+
+const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/search?key=${config.YOUTUBE_API_KEY}&type=video&q={QUERY}`;
+
+export const data = new SlashCommandBuilder().setName('stream').setDescription('stream a song in VC').addStringOption(option =>
+  option
+    .setName('query')
+    .setDescription('Provide a song URL')
+    .setRequired(true));
+
+export async function execute (interaction: CommandInteraction, client: Client): Promise<void> {
+  await interaction.deferReply();
+  const member = interaction.member as GuildMember;
+  const voiceChannel = member.voice.channel as VoiceChannel;
+  if (serverQueue.getTextChannel() === undefined) {
+    serverQueue.setTextChannel(interaction.channel as TextChannel);
+  }
+  let queryUrlOrString = interaction.options.getString('query');
+  if (queryUrlOrString === null) {
+    await interaction.editReply('please provide a url!');
+    return;
+  }
+  const queryIsKeywords = !ytdl.validateURL(queryUrlOrString) && typeof queryUrlOrString === 'string';
+  if (queryIsKeywords) {
+    const apiUrlWithQuery = youtubeApiUrl.replace('{QUERY}', queryUrlOrString);
+    const res = await fetch(apiUrlWithQuery);
+    const queryResults = await res.json() as youtubeResponse;
+    if (queryResults.items.length === 0) {
+      console.log('queryResults.items.length === 0');
+      await interaction.editReply('something went wrong while fetching the query results!');
+      return;
+    }
+    if (queryResults.items[0].id.videoId === null) {
+      console.log('queryResults.items[0].id.videoId === null');
+      await interaction.editReply(`invalid query result! ${JSON.stringify(queryResults)}`);
+      return;
+    }
+    queryUrlOrString = `https://www.youtube.com/watch?v=${queryResults.items[0].id.videoId}`;
+  }
+  const connection = await connectToChannel(voiceChannel);
+
+  const audioPlayerSubscription = connection.subscribe(player);
+
+  if (audioPlayerSubscription === undefined) {
+    await interaction.editReply('Unable to subscribe to AudioPlayer, please open a support ticket.');
+    return;
+  }
+
+  const newSong: Song = {
+    url: queryUrlOrString,
+    info: await ytdl.getInfo(queryUrlOrString),
+    requestingUser: interaction.user
+  };
+  serverQueue.addSongToQueue(newSong);
+  console.log(`added ${newSong.info.videoDetails.title} to queue. queue length: ${serverQueue.getQueuedSongs().length}`);
+  interaction.editReply(`Added **${newSong.info.videoDetails.title}** to queue! position in queue: ${serverQueue.getQueuedSongs().length}`).catch(console.error);
+  const shouldPlaySongImmediately: boolean = player.state.status === AudioPlayerStatus.Idle && serverQueue.getQueuedSongs().length > 0;
+  if (shouldPlaySongImmediately) {
+    const nextSong = serverQueue.getFirstSong();
+    if (nextSong === undefined) {
+      console.log('nextSong is undefined');
+      return;
+    }
+    const resource = getSongResourceByYouTubeUrl(nextSong.url);
+    player.play(resource);
+  }
+}
+
+interface youtubeResponse {
+  kind: string
+  etag: string
+  nextPageToken: string
+  regionCode: string
+  pageInfo: {
+    totalResults: number
+    resultsPerPage: number
+  }
+  items: youtubeResponseItem[]
+}
+
+interface youtubeResponseItem {
+  kind: string
+  etag: string
+  id: {
+    kind: string
+    videoId: string
+  }
+}
--- src/config.ts
+++ src/config.ts
@@ -2,17 +2,18 @@ import dotenv from 'dotenv';
 import { BitFieldResolvable, IntentsString } from 'discord.js';
 dotenv.config();
 
-const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env;
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN, YOUTUBE_API_KEY } = process.env;
 
 throwErrIfEnvironmentVarsAreMissing(process.env);
 
 export const config: Record<string, string> = {
   CLIENT_ID: CLIENT_ID ?? '',
   GUILD_ID: GUILD_ID ?? '',
-  DISCORD_TOKEN: DISCORD_TOKEN ?? ''
+  DISCORD_TOKEN: DISCORD_TOKEN ?? '',
+  YOUTUBE_API_KEY: YOUTUBE_API_KEY ?? ''
 };
 
-export const BOT_INTENTS: BitFieldResolvable<IntentsString, number> = ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'];
+export const BOT_INTENTS: BitFieldResolvable<IntentsString, number> = ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES', 'GUILD_VOICE_STATES'];
 
 function throwErrIfEnvironmentVarsAreMissing (environmentVars: NodeJS.ProcessEnv): void {
   Object.values(environmentVars).forEach(v => {
