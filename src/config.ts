5f9220f4454409aca077bda381aa8d03d889bd92
Kapeet <itamar246@gmail.com>
2023-02-16 21:31:58 +0200

    too strict

--- src/config.ts
+++ src/config.ts
@@ -4,18 +4,17 @@ dotenv.config();
 
 const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN, YOUTUBE_API_KEY } = process.env;
 
-throwErrIfEnvironmentVarsAreMissing(process.env);
-
-export const config: Record<string, string> = {
+export const config = {
   CLIENT_ID: CLIENT_ID ?? '',
   GUILD_ID: GUILD_ID ?? '',
   DISCORD_TOKEN: DISCORD_TOKEN ?? '',
   YOUTUBE_API_KEY: YOUTUBE_API_KEY ?? ''
 };
+throwErrIfEnvironmentVarsAreMissing(config);
 
 export const BOT_INTENTS: BitFieldResolvable<IntentsString, number> = ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES', 'GUILD_VOICE_STATES'];
 
-function throwErrIfEnvironmentVarsAreMissing (environmentVars: NodeJS.ProcessEnv): void {
+function throwErrIfEnvironmentVarsAreMissing (environmentVars: NodeJS.ProcessEnv) {
   Object.values(environmentVars).forEach(v => {
     if (v === null || v === undefined) {
       throw new Error('missing environment variables');
