88fdd0d269672661a687366831a454220fb8060b
Kapeet <itamar246@gmail.com>
2023-06-10 12:58:04 +0300

    fix prettier config file name

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+	plugins: [require("prettier-plugin-sh")],
+	pluginSearchDirs: false,
+	tabWidth: 4,
+	printWidth: 110,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	quoteProps: "consistent",
+	trailingComma: "all",
+	bracketSpacing: true,
+	arrowParens: "always",
+	bracketSameLine: false,
+	indent: 4,
+};
