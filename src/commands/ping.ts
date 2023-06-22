ffcad6c23c81a2896587cccd17e9c59f86b59a65
Kapeet <itamar246@gmail.com>
2023-06-22 13:22:17 +0300

    init fixes

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,7 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 
 export const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with pong');
 
 export async function execute(interaction: CommandInteraction): Promise<void> {
-	return await interaction.reply(`Pong! latency: ${Date.now() - interaction.createdTimestamp} ms`);
+	await interaction.reply(`Pong! latency: ${Date.now() - interaction.createdTimestamp} ms`);
 }
