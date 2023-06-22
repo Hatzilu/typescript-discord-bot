d9846d249697f95f56f8aee61c89f770abe98491
Kapeet <itamar246@gmail.com>
2023-06-22 15:08:43 +0300

    fix ping cmd

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,6 +2,6 @@ import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 
 export const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with pong');
 
-export async function execute(interaction: CommandInteraction): Promise<void> {
-	await interaction.reply(`Pong! latency: ${Date.now() - interaction.createdTimestamp} ms`);
+export async function execute(interaction: CommandInteraction) {
+	await interaction.reply(`Pong! latency: ${Math.abs(Date.now() - interaction.createdTimestamp)}`);
 }
