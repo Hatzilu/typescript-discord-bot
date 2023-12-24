3938392613e3edc394b508b959c8bd9e2265e541
PawsOfDestruction <purvanjaro2@gmail.com>
2023-12-24 23:48:24 +0200

    unused

--- src/commands/greet.ts
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
-
-export const data = new SlashCommandBuilder().setName('greet').setDescription('Holiday-appropriate greeting');
-
-export async function execute(interaction: CommandInteraction): Promise<void> {
-	const hebrewText: string = 'ראש השנה שמח';
-	await interaction.reply(`${interaction.user.toString()}, ${hebrewText}`);
-}
