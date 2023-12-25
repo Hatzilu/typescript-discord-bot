93310ef4ffa69d49d81314d3b5992a2228797ef4
PawsOfDestruction <purvanjaro2@gmail.com>
2023-12-25 15:48:22 +0200

    fix clear command crash

--- src/commands/music/clear.ts
+++ src/commands/music/clear.ts
@@ -18,6 +18,6 @@ export async function execute(interaction: ChatInputCommandInteraction, client: 
 		return;
 	}
 
-	queue.songs = [];
+	queue.songs.splice(1, queue.songs.length);
 	await interaction.reply('💥 Queue has been cleared.').catch(console.error);
 }
